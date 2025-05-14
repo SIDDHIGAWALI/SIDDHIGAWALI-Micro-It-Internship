e895d5fde9429189ec7d5259a55a481a63dc862f
SIDDHIGAWALI <166315390+SIDDHIGAWALI@users.noreply.github.com>
2025-05-14 11:17:24 +0530

    Update Tic_Tac_Toe.java

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,1 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class TicTacToeGame extends JFrame implements ActionListener {
+private JButton[] buttons;
+private boolean isX = true;
+public TicTacToeGame() {
+ buttons = new JButton[9];
+ setTitle("Tic Tac Toe");
+ setLayout(new GridLayout(3, 3));
+ for (int i = 0; i < 9; i++) {
+ buttons[i] = new JButton("");
+ buttons[i].setFont(new Font("Arial", Font.PLAIN, 80));
+ buttons[i].setFocusPainted(false);
+ buttons[i].addActionListener(this);
+add(buttons[i]);
+ }
 
+ setSize(300, 300);
+ setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ setLocationRelativeTo(null);
+ setVisible(true);
+ }
+
+public void actionPerformed(ActionEvent e) {
+ JButton buttonClicked = (JButton) e.getSource();
+ if (isX) {
+ buttonClicked.setText("X");
+ } else {
+
+ buttonClicked.setText("O");
+ }
+
+ buttonClicked.setEnabled(false);
+ isX = !isX;
+  checkWin();
+ }
