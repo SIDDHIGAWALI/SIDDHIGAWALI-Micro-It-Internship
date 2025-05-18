a87a05d7696d8d10a383e82df04090c5d2637b36
SIDDHIGAWALI <166315390+SIDDHIGAWALI@users.noreply.github.com>
2025-05-18 11:01:02 +0530

    Update Tic_Tac_Toe.java

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -27,11 +27,56 @@ public void actionPerformed(ActionEvent e) {
  if (isX) {
  buttonClicked.setText("X");
  } else {
-
  buttonClicked.setText("O");
  }
-
  buttonClicked.setEnabled(false);
  isX = !isX;
   checkWin();
  }
+ private void checkWin() {
+ int[][] winCombinations = {
+ {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+ {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+ {0, 4, 8}, {2, 4, 6}
+ };
+
+ for (int[] combination : winCombinations) {
+ int a = combination[0];
+ int b = combination[1];
+ int c = combination[2];
+ if(buttons[a].getText().equals(buttons[b].getText())&& 
+buttons[a].getText().equals(buttons[c].getText())&& 
+!buttons[a].getText().isEmpty()) {
+ JOptionPane.showMessageDialog(this, buttons[a].getText() + " 
+wins!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+ resetGame();
+ return;
+ }
+ }
+ boolean isDraw = true;
+ for (JButton button : buttons) {
+ if (button.getText().isEmpty()) {
+ isDraw = false;
+ break;
+ }
+ }
+ if (isDraw) {
+ JOptionPane.showMessageDialog(this, "It's a draw!", "Game Over", 
+JOptionPane.INFORMATION_MESSAGE);
+ resetGame();
+ }
+ }
+ private void resetGame() {
+ for (JButton button : buttons) {
+ button.setText("");
+ button.setEnabled(true);
+ }
+
+ isX = true;
+ }
+
+public static void main(String[] args) {
+ TicTacToeGame t=new TicTacToeGame();
+ }
+
+}
